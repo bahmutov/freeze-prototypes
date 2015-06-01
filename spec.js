907f405003aa17e22b5aa59a364e7701a3044772
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-06-01 13:54:12 -0400

    testing overwriting prototype

--- spec.js
+++ spec.js
@@ -6,4 +6,15 @@ describe('freeze-prototypes', function () {
     Array.prototype.foo = 'foo';
     la(typeof Array.prototype.foo === 'undefined');
   });
+
+  it('stops modifying properties', function () {
+    var concat = Array.prototype.concat;
+    Array.prototype.concat = function fakeConcat() {
+      throw new Error('hi from concat');
+    };
+    la(Array.prototype.concat === concat, 'still old concat');
+    var result = [].concat(['foo']);
+    la(result.length === 1 &&
+      result[0] === 'foo', 'used concat');
+  });
 });
